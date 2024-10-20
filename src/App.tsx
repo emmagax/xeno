8ece078f8555b3604424144da890c7bc307a5565
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-10-19 19:49:06 -0600

    xeno 1.0

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/styles.scss"; // If you're using Sass for styles
+
+// Import any components you create
+
+const App: React.FC = () => {
+  return (
+    <div className="header">
+      <h1>Xeno</h1>
+    </div>
+  );
+};
+
+export default App;
